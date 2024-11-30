d58fa55c027fc03212676c368a01f6ce32a44799
HunkyCapybara <162281341+HunkyCapybara@users.noreply.github.com>
2024-11-30 20:04:56 +0530

    Update jenkins.sh

--- Terraform-Jenkins/jenkins.sh
+++ Terraform-Jenkins/jenkins.sh
@@ -2,7 +2,6 @@
 sudo yum install git -y
 sudo yum install java-17-amazon-corretto -y
 sudo yum install maven -y
-
 sudo wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.97/bin/apache-tomcat-9.0.97.tar.gz
 sudo tar -xvzf apache-tomcat-9.0.97.tar.gz #untar
 cd apache-tomcat-9.0.97/
